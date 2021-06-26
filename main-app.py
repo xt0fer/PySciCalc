c52c58a76e77ca0688e7d6a337b3b6e90536aecc
Chuck <chuck@zipcodes-MacBook-Pro-chuck.local>
2021-06-26 17:19:49 -0400

    working on display

--- main-app.py
+++ main-app.py
@@ -1,9 +1,19 @@
 from calculator import Calculator
 
 #chuck was here
+
+
 def getTwoNumbers():
-    a = float(input("first number? "))
-    b = float(input("second number? "))
+    a = input("first number? ")
+    if a == 'display':
+        a = currentState()
+    else:
+        a = float(a)
+    b = input("second number? ")
+    if b == 'display':
+        b = currentState()
+    else:
+        b= float(b)
     return a, b
 
 def getOneNumber():
@@ -11,14 +21,23 @@ def getOneNumber():
     return a
 
 def displayResult(x: float):
-    print(x, "\n")
+    print(x)
 
+def currentState():
+    return 
 
 def performCalcLoop(calc):
     while True:
-        choice = input("Operation? ")
+        choice = input("\n: ").lower()
         if choice == 'q':
             break  # user types q to quit calulator.
+        elif choice == 'help':
+            print("List of commands:")
+            print("'q' for quit")
+            print("'clear' set display to 0")
+            print("Operations: 'add', 'subtract', 'multiply', 'divide', 'square', 'square root'")
+        elif choice == 'clear':
+            displayResult(0)    
         elif choice == 'add':
             a, b = getTwoNumbers()
             displayResult(calc.add(a, b))
@@ -40,15 +59,22 @@ def performCalcLoop(calc):
 
         else:
             print("That is not a valid input.")
+        
+        
+        
 
 
 # main start
 def main():
-    
+    current_display_result = 0
     calc = Calculator()
-    print('this is the best calculator :)')
+    print('\n\nthis is the best calculator :)\n')
     displayResult(0)
+    
+    
     performCalcLoop(calc)
+    
+    
     print("Done Calculating.")
 
 
