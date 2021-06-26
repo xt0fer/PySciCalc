54fe790934c86e7b3d7b9e0f41b17943f1159e03
Chuck <chuck@zipcodes-MacBook-Pro-chuck.local>
2021-06-25 20:10:13 -0400

    square and square root added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Calculator:
 
     def __init__(self):
@@ -14,5 +16,11 @@ class Calculator:
 
     def divide(self, x, y):
         return x / y
+
+    def square(self, x):
+        return x * x
+
+    def square_root(self, x):
+        return math.sqrt(x)
 
 # add lots more methods to this calculator class.
--- main-app.py
+++ main-app.py
@@ -6,6 +6,8 @@ def getTwoNumbers():
     b = float(input("second number? "))
     return a, b
 
+def getOneNumber():
+    a = float(input('number? '))
 
 def displayResult(x: float):
     print(x, "\n")
@@ -28,14 +30,23 @@ def performCalcLoop(calc):
         elif choice == 'divide':
             a, b = getTwoNumbers()
             displayResult(calc.divide(a, b))
+        elif choice == 'square':
+            a, = getOneNumber()
+            displayResult(calc.square(a, ))
+        elif choice == 'squart root':
+            a,  = getOneNumber()
+            displayResult(calc.square_root(a, ))
+
         else:
             print("That is not a valid input.")
 
 
 # main start
 def main():
+    
     calc = Calculator()
     print('this is the best calculator :)')
+    displayResult(0)
     performCalcLoop(calc)
     print("Done Calculating.")
 
