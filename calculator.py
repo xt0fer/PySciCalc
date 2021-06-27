e7d606a6fea5faf9518ec38d4fb0bf56fddd54b1
AidanFarhi <aidanmcfarhi@gmail.com>
2021-06-27 08:31:35 -0400

    separated one argument tests and two argument tests

--- calculator.py
+++ calculator.py
@@ -1,12 +1,71 @@
+import math
+
+
 class Calculator:
 
     def __init__(self):
-        pass
+        self.__state = 0
 
     def add(self, x, y):
         return x + y
 
-    def sub(self, x, y):
-        return 0
+    def subtract(self, x, y):
+        return x - y
+
+    # MY METHODS
+    def set_state(self, x):
+        self.__state = x
+
+    def get_state(self):
+        return self.__state
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x / y
+
+    def square(self, x):
+        return x * x
+
+    def square_root(self, x):
+        return math.sqrt(x)
+    
+    def inverse(self, x):
+        return 1 / x
+
+    def sine(self, x):
+        return math.sin(x)
+
+    def cosine(self, x):
+        return math.cos(x)
+
+    def tangent(self, x):
+        return math.tan(x)
+
+    def inverse_sine(self, x):
+        return math.asin(x)
+
+    def inverse_cosine(self, x):
+        return math.acos(x)
+
+    def inverse_tangent(self, x):
+        return math.atan(x)
+
+    def factorial(self, x):
+        if abs(x) > 0:
+           return abs(x) * self.factorial(abs(x) - 1)
+        else:
+            return 1
+
+    def switch_sign(self, x):
+        return -x
+    
+    def natural_log(self, x):
+        return math.log(x)
+
+    def log(self, x, y):
+        return math.log(x, y)
+
 
 # add lots more methods to this calculator class.
---
+++ test/runtests/two_arg_calc_tests.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+from test.error_colors import ErrorColors
+from calculator import Calculator
+
+
+class TestCalcTwoArgs(TestCase):
+
+    def _test(self, method, tests):
+        for num1, num2, expected in tests:
+            actual = method(num1, num2)
+            error_message = self.__return_calc_error(num1, num2, expected, actual)
+            self.assertEqual(expected, actual, error_message)
+    
+    def __return_calc_error(self, n1, n2, exp, act):
+        result = f"""
+        {ErrorColors.GREEN}Expected: ({n1}, {n2}) -> {exp}
+        {ErrorColors.RED}Actual:   ({n1}, {n2}) -> {act}{ErrorColors.RESET}
+        """
+        return result
+
+    # TWO ARG TESTS
+    def test_add(self):
+        c = Calculator()
+        self._test(c.add, [
+            (1, 2, 3), (2, 3, 5), (6, 5, 11), (7, 10, 17), 
+            (8, 9, 17), (31, 2, 33), (-13, 3, -10), (-5, -5, -10)
+        ])
+
+    def test_subtract(self):
+        c = Calculator()
+        self._test(c.subtract, [
+            (1, 2, -1), (4, 3, 1), (10, 5, 5), (67, 10, 57),
+            (-5, -10, 5), (31, 2, 29), (-13, 3, -16), (-6, 2, -8)
+        ])
+
+    def test_multiply(self):
+        c = Calculator()
+        self._test(c.multiply, [
+            (1, 2, 2), (4, 3, 12), (10, 0, 0), (30, 3, 90),
+            (-5, -10, 50), (31, 2, 62), (-11, 3, -33), (-6, 2, -12)
+        ])
+
+    def test_divide(self):
+        c = Calculator()
+        self._test(c.divide, [
+            (1, 2, 0.5), (12, 3, 4), (9, 2, 4.5), (7, 7, 1),
+            (-10, 10, -1), (30, -3, -10), (-200, -2, 100), (-6, 2, -3)
+        ])
+
+    def test_log(self):
+        c = Calculator()
+        self._test(c.log, [
+            (1,3,0.0),(2,2,1.0),(3,4,0.7924812503605781),(20,6,1.6719500161730103),(35,20,1.1868043392514251),
+            (123,7,2.4729735634035324),(54,4,2.8774437510817346),(525,3,5.701190790597276),(262,5,3.4598069678497754),
+            (100,89,1.0259619640926507)         
+        ])
