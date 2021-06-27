f25ba89eb7ed135389845cefa7c2a7ee0a3a90f6
Chuck <chuck@zipcodes-MacBook-Pro-chuck.local>
2021-06-27 12:53:04 -0400

    fixed the state, added err

--- main-app.py
+++ main-app.py
@@ -3,19 +3,18 @@ from calculator_scientific import sci_Calculator
 
 #chuck was here
 #global variable for display (use global if needed in function)
-current_state = 0
-
+calculator = Calculator()
 def getTwoNumbers():
 
-    global current_state
+    
     a = input("first number? ")
     if a == 'display':
-        a = current_state
+        a = calculator.current_state
     else:
         a = float(a)
     b = input("second number? ")
     if b == 'display':
-        b = current_state
+        b = calculator.current_state
     else:
         b= float(b)
     return a, b
@@ -23,23 +22,24 @@ def getTwoNumbers():
 def getOneNumber():
     #trig functions must be in radians with math.trig functions
     #want to store a radian into a, and if it's a degree, convert it to radian
-    global current_state
+    
     a = input('please enter value: ')
     if a == 'display':
-        a = current_state
+        a = calculator.current_state
     else:
         a = float (a)
     return a
 
 
 def displayResult(x):
-    global current_state
+    
     
     if x == 'err':
-        print(x)
+        print('---',x,'---')
+        calculator.current_state = x
     else:
         print('---',float(x),'---')
-        current_state = float(x) 
+        calculator.current_state = float(x)
 
 
 def switchUnitsMode():
@@ -51,8 +51,17 @@ def switchUnitsMode():
      
 
 def performCalcLoop(calc):
-    global current_state
+    
     while True:
+        if calculator.current_state == 'err':
+            choice = input("\n: ").lower()
+            if choice == 'clear':
+                displayResult(0)
+            elif choice == 'q':
+                break
+            else:
+                print('---',calculator.current_state,'---')
+                print('clear display')
 
         choice = input("\n: ").lower()
         
@@ -68,58 +77,46 @@ def performCalcLoop(calc):
             print("Operations: 'add', 'subtract', 'multiply', 'divide', 'square', 'square root', 'inverse', factorial")
         elif choice == 'clear':
             displayResult(0)
-            current_state = 0    
+            calculator.current_state = 0    
 
         #trig functions    
         elif choice == 'sin':
             a = getOneNumber()
             displayResult(calc.sin(a))
-            current_state = calc.sin(a)
         elif choice == 'cos':
             a = getOneNumber()
             displayResult(calc.cos(a))
-            current_state = calc.cos(a)
         elif choice == 'tan':
             a = getOneNumber()
             displayResult(calc.tan(a))
-            current_state = calc.tan(a)
         elif choice == 'inverse sin':
             a = getOneNumber()
             displayResult(calc.inverse_sin(a))
-            current_state = calc.inverse_sin(a)
         elif choice == 'inverse cos':
             a = getOneNumber()
             displayResult(calc.inverse_cos(a))
-            current_state = calc.inverse_cos(a)
         elif choice == 'inverse tan':
             a = getOneNumber()
             displayResult(calc.inverse_tan(a))
-            current_state = calc.inverse_tan(a)
-
+        #basic functions
         elif choice == 'add':
             a, b = getTwoNumbers()
             displayResult(calc.add(a, b))
-            current_state = calc.add(a,b)
         elif choice == 'subtract':
             a, b = getTwoNumbers()
             displayResult(calc.subtract(a, b))
-            current_state = calc.subtract(a,b)
         elif choice == 'multiply':
             a, b = getTwoNumbers()
             displayResult(calc.multiply(a, b))
-            current_state = calc.multiply(a,b)
         elif choice == 'divide':
             a, b = getTwoNumbers()
             displayResult(calc.divide(a, b))
-            current_state = calc.divide(a,b)
         elif choice == 'square':
             a = getOneNumber()
             displayResult(calc.square(a))
-            current_state = calc.square(a)
         elif choice == 'square root':
             a = getOneNumber()
             displayResult(calc.square_root(a))
-            current_state = calc.square_root(a)
         elif choice == 'inverse':
             a = getOneNumber
             displayResult()
@@ -137,6 +134,7 @@ def main():
     calc = Calculator()
     print('\n\nthis is the best calculator :)\n')
     displayResult(0)
+    
     
     
     performCalcLoop(calc)
